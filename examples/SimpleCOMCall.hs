21b34ec9baa04b3919a3ae6dac8844bba531b396
Niklas Larsson <niklas@mm.st>
2017-07-16 13:53:04 +0200

    Flesh out IUserNotification2 interface in example

--- examples/SimpleCOMCall.hs
+++ examples/SimpleCOMCall.hs
@@ -7,6 +7,8 @@ import Foreign.Ptr
 import Foreign.C.String
 import Foreign.C.Types
 
+import Graphics.Win32.GDI.Types
+
 import System.Win32.Com
 import System.Win32.Types hiding (HRESULT)
 
@@ -16,31 +18,53 @@ main :: IO ()
 main = coRun $ do
         i <- coCreateInstance clsidUserNotification2 
                     Nothing CLSCTX_INPROC_SERVER iidUserNotification2
-        _ <- setBalloonInfo i "Hello from hcom" "Hello" 3
+        setBalloonInfo i "Hello from hcom" "Hello" 1
+        playSound i "Asterisk"
+        setBalloonRetry i 5000 2000 3
         hr <- showBalloon i nullPtr 10000 nullPtr
-        if hr == s_OK then putStrLn "OK!" else putStrLn "BOO!"
-
+        putStrLn "Done!"
         
 -- The User notification interface
 
-data UserNotification2 a = Un
+data UserNotification2 a
 type IUserNotification2 a = IUnknown (UserNotification2 a)
 
 iidUserNotification2 = mkIID  "{215913cc-57eb-4fab-AB5A-E5FA7BEA2A6C}"
 clsidUserNotification2 = mkCLSID "{0010890e-8789-413c-adbc-48f5b511b3af}"
 
-setBalloonInfo :: IUnknown (IUserNotification2 a) -> String -> String -> DWORD -> IO HRESULT
+setBalloonInfo :: IUnknown (IUserNotification2 a) -> String -> String -> DWORD -> IO ()
 setBalloonInfo iptr title text flags = do
     -- don't know what windows does with memory so leak it
     titleWStr <- newCWString title
     textWStr <- newCWString text
-    invokeIt (\meth ip -> prim_setBalloonInfo meth ip titleWStr textWStr flags) 3 iptr
+    invokeAndCheck (\meth ip -> prim_setBalloonInfo meth ip titleWStr textWStr flags) 3 iptr
 
+setBalloonRetry :: IUnknown (IUserNotification2 a) -> DWORD -> DWORD -> UINT -> IO ()
+setBalloonRetry iptr showTime interval retryCount = do
+    invokeAndCheck (\meth ip -> prim_setBallonRetry meth ip showTime interval retryCount) 4 iptr
+
+setIconInfo :: IUnknown (IUserNotification2 a) -> HICON -> String -> IO ()
+setIconInfo iptr icon tooltip = do
+    tooltipWStr <- newCWString tooltip
+    invokeAndCheck (\meth ip -> prim_setIconInfo meth ip icon tooltipWStr) 5 iptr
 
 showBalloon :: IUnknown (IUserNotification2 a) -> Ptr () -> DWORD -> Ptr () -> IO HRESULT
 showBalloon iptr iqcont interval cb = do
     invokeIt (\meth ip -> prim_showBalloon meth ip iqcont interval cb) 6 iptr
 
-foreign import stdcall "dynamic" prim_setBalloonInfo :: Ptr (Ptr() -> Ptr CWchar -> Ptr CWchar -> DWORD -> IO HRESULT) -> Ptr () -> Ptr CWchar -> Ptr CWchar -> DWORD -> IO HRESULT
+playSound :: IUnknown (IUserNotification2 a) -> String -> IO ()
+playSound iptr sound = withCWString sound $
+                              (\s -> invokeAndCheck
+                                         (\meth ip -> prim_playSound meth ip s) 7 iptr)
+
+-- FFI declarations
+
+foreign import stdcall "dynamic" prim_setBalloonInfo :: Ptr (Ptr() -> LPCWSTR -> LPCWSTR -> DWORD -> IO HRESULT) -> Ptr () -> LPCWSTR -> LPCWSTR -> DWORD -> IO HRESULT
+
+foreign import stdcall "dynamic" prim_setBallonRetry :: Ptr (Ptr() -> DWORD -> DWORD -> UINT -> IO HRESULT) -> Ptr() -> DWORD -> DWORD -> UINT -> IO HRESULT
+
+foreign import stdcall "dynamic" prim_setIconInfo :: Ptr (Ptr () -> HICON -> LPCWSTR -> IO HRESULT) -> Ptr () -> HICON -> LPCWSTR -> IO HRESULT
 
 foreign import stdcall "dynamic" prim_showBalloon :: Ptr (Ptr () -> Ptr () -> DWORD -> Ptr() -> IO HRESULT) -> Ptr () -> Ptr () -> DWORD -> Ptr () -> IO HRESULT
+
+foreign import stdcall "dynamic" prim_playSound :: Ptr (Ptr () -> LPCWSTR -> IO HRESULT) -> Ptr () -> LPCWSTR -> IO HRESULT
